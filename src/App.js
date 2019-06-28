c1b9ed241b8d427e984c348e0c1f3065496d9d02
Ben Cohen <benjcohen1985@gmail.com>
2019-06-28 16:01:37 -0400

    Import component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-
-function App() {
-  return <div className="App" />;
+import Navbar from "./Navbar";
+import "./Navbar.css";
+import Footer from "./Footer";
+import "./Footer.css";
+import LandingPage from "./LandingPage";
+import "./LandingPage.css";
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+          <LandingPage />
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
