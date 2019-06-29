d847d78551af0d757d1b5d52c091ae9cb0d7d584
Ben Cohen <benjcohen1985@gmail.com>
2019-06-29 11:36:28 -0400

    Create thank you page

---
+++ src/ThankYouPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function ThankYouPage() {
+  return (
+    <div className="hero">
+      <h1>Thank You!</h1>
+      <div className="redirected">
+        <h3>You will be redirected in a few seconds...</h3>
+      </div>
+    </div>
+  );
+}
