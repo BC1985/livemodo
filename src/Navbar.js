5dbcd871b8422fb3302caede0e34eb6cb499dfe5
Ben Cohen <benjcohen1985@gmail.com>
2019-06-29 20:29:34 -0400

    Add links to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,12 +3,23 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="nav-wrapper">
-      <div className="container">
-        <Link to="/" className="logo">
-          Navbar
-        </Link>
-      </div>
-    </nav>
+    <div className="container">
+      <nav className="nav-wrapper">
+        <ul>
+          <Link to="/">
+            <li>About</li>
+          </Link>
+          <Link to="/add">
+            <li>Add review</li>
+          </Link>
+          <Link to="/browse">
+            <li>Browse reviews</li>
+          </Link>
+          <Link to="/login">
+            <li>Log in</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
   );
 }
