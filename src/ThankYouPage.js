819db4310035d8dc7b542967a405b74f753cb25e
Ben Cohen <benjcohen1985@gmail.com>
2019-07-03 08:41:08 -0400

    Remove unfunctional code

--- src/ThankYouPage.js
+++ src/ThankYouPage.js
@@ -1,26 +1,9 @@
 import React, { Component } from "react";
-// import { Redirect } from "react-router-dom";
+// import { Router, Route } from "react-router-dom";
+// import LandingPage from "./LandingPage";
 
 export default class ThankYouPage extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       Redirect: false
-  //     };
-  //   }
-
-  //   componentDidMount() {
-  //     setTimeout(
-  //       this.setState({
-  //         Redirect: true
-  //       }),
-  //       3000
-  //     );
-  //   }
   render() {
-    // if (this.state.Redirect) {
-    //   return <Redirect to="/" />;
-    // }
     return (
       <div className="hero">
         <h1>Thank You!</h1>
