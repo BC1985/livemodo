7b0f0db4b02fd5437c557634f84b63de5690ea55
Ben Cohen <benjcohen1985@gmail.com>
2019-07-07 11:10:24 -0400

    Add logic for redirecting and add button

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+// import LandingPage from "./LandingPage";
+import Button from "./Button";
 
-const handleSubmit = e => {
-  e.preventDefault();
-};
-export default function LoginForm() {
-  return (
-    <div className="login-form-container">
-      <section className="login-header">
-        <h1>Login to Livemodo</h1>
-      </section>
-      <form onSubmit={handleSubmit}>
-        <label>Username</label>
-        <input type="text" />
-        <label>Password</label>
-        <input type="text" />
-        <button type="submit">Log in</button>
-      </form>
-      <section className="forgot-credentials">
-        <p>Forgot username/ password? Click here</p>
-      </section>
-    </div>
-  );
+export default class LoginForm extends Component {
+  constructor() {
+    super();
+    this.state = {
+      Redirect: false
+    };
+  }
+  render() {
+    const handleSubmit = e => {
+      e.preventDefault();
+      this.setState({
+        Redirect: true
+      });
+    };
+    if (this.state.Redirect) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div className="login-form-container">
+        <section className="login-header">
+          <h1>Login to Livemodo</h1>
+        </section>
+        <form onSubmit={handleSubmit}>
+          <label>Username</label>
+          <input type="text" />
+          <label>Password</label>
+          <input type="text" />
+
+          <Button>Log in</Button>
+        </form>
+        <section className="forgot-credentials">
+          <p>Forgot username/ password? Click here</p>
+        </section>
+      </div>
+    );
+  }
 }
