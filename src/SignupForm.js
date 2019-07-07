c74b50c5adf9b26af83d6b580434808639e4cd8c
Ben Cohen <benjcohen1985@gmail.com>
2019-07-07 11:09:29 -0400

    Split form input into divs

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -12,35 +12,47 @@ function SignupForm() {
         <h3>Join the Livemodo community!</h3>
       </div>
       <div className="signup-form">
-        <form>
-          <label>*First Name</label>
-          <input type="text" placeholder="e.g Tommy" required />
-          <label>*Last Name</label>
-          <input type="text" placeholder="e.g Wisseau" required />
-          <label>*Username</label>
-          <input type="text" required />
-          <label>*Email</label>
-          <input type="text" placeholder="e.g tommy@theroom.com" required />
-          <label>*Password</label>
-          <input type="text" required />
+        <form id="signup">
+          <div className="form-text-input">
+            <label>*First Name</label>
+            <input type="text" placeholder="e.g Tommy" required />
+          </div>
+          <div className="form-text-input">
+            <label>*Last Name</label>
+            <input type="text" placeholder="e.g Wisseau" required />
+          </div>
+          <div className="form-text-input">
+            <label>*Username</label>
+            <input type="text" required />
+          </div>
+          <div className="form-text-input">
+            <label>*Email</label>
+            <input type="text" placeholder="e.g tommy@theroom.com" required />
+          </div>
+          <div className="form-text-input">
+            <label>*Password</label>
+            <input type="text" required />
+          </div>
           <p style={spanStyle}>(* indicates required field)</p>
           <p>
             Pick the option that best describes your concergoing habits{" "}
             <span style={spanStyle}>(optional)</span>
           </p>
-          <div>
-            <input type="radio" value="homebody" />
-            Homebody
+          <div className="form-buttons">
+            <div>
+              <input type="radio" value="homebody" />
+              Homebody
+            </div>
+            <div>
+              <input type="radio" />
+              Occasional concertgoer
+            </div>
+            <div>
+              <input type="radio" />
+              Concert Connuiseur
+            </div>
+            <button type="submit">Sign me up!</button>
           </div>
-          <div>
-            <input type="radio" />
-            Occasional concertgoer
-          </div>
-          <div>
-            <input type="radio" />
-            Concert Connuiseur
-          </div>
-          <button type="submit">Sign me up!</button>
         </form>
       </div>
     </>
