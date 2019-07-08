33bb28dc220cfdf42a0d910aaf2882b156844e5d
Ben Cohen <benjcohen1985@gmail.com>
2019-07-08 10:43:25 -0400

    Add push div to keep footer at the bottom

--- src/ThankYouPage.js
+++ src/ThankYouPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import "./ThankYouPage.css";
 // import LandingPage from "./LandingPage";
 
 class ThankYouPage extends Component {
@@ -8,17 +9,20 @@ class ThankYouPage extends Component {
       this.props.history.push("/");
       this.props.thankYouRedirect();
     };
-    setTimeout(redirect, 3000);
+    setTimeout(redirect, 9000);
   }
 
   render() {
     return (
-      <div className="hero">
-        <h1>Thank You!</h1>
-        <div className="redirected">
-          <h3>You will be redirected in a few seconds...</h3>
+      <>
+        <div className="hero">
+          <h1>Thank You!</h1>
+          <div className="redirected">
+            <h3>You will be redirected in a few seconds...</h3>
+          </div>
         </div>
-      </div>
+        <div className="push" />
+      </>
     );
   }
 }
