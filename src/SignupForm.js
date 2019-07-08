6e2875e6a714424044484009b4758e1a7b0a47b5
Ben Cohen <benjcohen1985@gmail.com>
2019-07-08 09:31:34 -0400

    Split inputs into divs

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -10,30 +10,31 @@ function SignupForm() {
   return (
     <>
       <div className="call-to-action">
-        <h3>Join the Livemodo community!</h3>
+        <h2>Join the Livemodo community!</h2>
       </div>
       <div className="signup-form">
         <form id="signup">
-          <div className="form-text-input">
-            <label>*First Name</label>
+          <div className="text-input">
+            <label>*First Name </label>
             <input type="text" placeholder="e.g Tommy" required />
           </div>
-          <div className="form-text-input">
-            <label>*Last Name</label>
+          <div className="text-input">
+            <label>*Last Name </label>
             <input type="text" placeholder="e.g Wisseau" required />
           </div>
-          <div className="form-text-input">
-            <label>*Username</label>
+          <div className="text-input">
+            <label>*Username </label>
             <input type="text" required />
           </div>
-          <div className="form-text-input">
-            <label>*Email</label>
+          <div className="text-input">
+            <label>*Email </label>
             <input type="text" placeholder="e.g tommy@theroom.com" required />
           </div>
-          <div className="form-text-input">
+          <div className="text-input">
             <label>*Password</label>
             <input type="text" required />
           </div>
+
           <p style={spanStyle}>(* indicates required field)</p>
           <p>
             Pick the option that best describes your concergoing habits{" "}
