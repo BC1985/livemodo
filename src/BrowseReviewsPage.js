8d53877d3233546d79546a2fd670376b578a1ae6
Ben Cohen <benjcohen1985@gmail.com>
2019-07-09 19:51:55 -0400

    Render data from backend

--- src/BrowseReviewsPage.js
+++ src/BrowseReviewsPage.js
@@ -1,98 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
-import { faPortrait } from "@fortawesome/free-solid-svg-icons";
+// import { faPortrait } from "@fortawesome/free-solid-svg-icons";
 import "./BrowseReviewsPage.css";
+import { parseDate } from "./utils/parseDate";
 
-function BrowseReviewsPage() {
-  const star = <FontAwesomeIcon icon={faStar} />;
-  const userThumbnail = <FontAwesomeIcon icon={faPortrait} />;
-
-  return (
-    <div className="browse-reviews-container">
-      <div className="reviews-section">
-        <h1>Browse latest reviews</h1>
-        <div className="review-container">
-          <h3 className="band-name">Sonic Tooth</h3>
-          <p>Venue: The Globe Theatre</p>
-          <p>Date:12/3/2003</p>
-          {star}
-          {star}
-          {star}
-          {star}
-          <h4 className="tagline">
-            This is a user tagline lorem ipsum dolor sit amet
-          </h4>
-
-          <p className="user-name">
-            <span className="user-thumbnail">{userThumbnail}</span> Username{" "}
-            <span className="user-status">(avid concertgoer)</span>
-          </p>
-          <div className="review">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a
-              orci tincidunt, rhoncus ipsum in, tincidunt est. Fusce aliquam
-              quam nec vulputate varius. In hac habitasse platea dictumst.
-              Vivamus vulputate rhoncus ipsum sollicitudin maximus. Pellentesque
-              id risus efficitur, egestas odio sit amet, ege
-            </p>
-          </div>
+class BrowseReviewsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviews: []
+    };
+  }
+  componentDidMount() {
+    this.fetchReviews();
+  }
+  fetchReviews = async () => {
+    const data = await fetch("http://localhost:8000/api/reviews");
+    const reviews = await data.json();
+    this.setState({
+      reviews
+    });
+    console.log(this.state);
+  };
+  render() {
+    const star = <FontAwesomeIcon icon={faStar} />;
+    // const userThumbnail = <FontAwesomeIcon icon={faPortrait} />;
+    const { reviews } = this.state;
+    return (
+      <>
+        <div>
+          {reviews.map(review => (
+            <div key={review.id}>
+              <h2>{review.band_name}</h2>
+              <h3>{review.tagline}</h3>
+              <p>Venue: {review.venue}</p>
+              <p>Date: {parseDate(review.show_date)}</p>
+              <p>posted: {parseDate(review.posted)}</p>
+              <p>{review.content}</p>
+              <p>{star}</p>
+            </div>
+          ))}
         </div>
-        <div className="review-container">
-          <h3 className="band-name">The Pointy Brothers</h3>
-          <p className="venue-name">Venue: Bob's Crab Shack</p>
-          <p>Date:05/11/2012</p>
-
-          {star}
-          {star}
-          <h4 className="tagline">
-            This is a user tagline lorem ipsum dolor sit amet
-          </h4>
-          <p className="user-name">
-            <span className="user-thumbnail">{userThumbnail}</span> Username{" "}
-            <span className="user-status">(Homebody)</span>
-          </p>
-          <div className="review">
-            <p>
-              magnis dis parturient montes, nascetur ridiculus mus. Quisque in
-              lorem metus. Aenean dolor massa, varius vitae rutrum sed, congue
-              at urna. Proin et ex at velit feugiat finibus at eget nunc.
-              Suspendisse maximus, lacus eget euismod vulputate, lacus mi
-              pharetra ante, sed porta lacus leo ac orci
-            </p>
-          </div>
-        </div>
-        <div className="review-container">
-          <h3 className="band-name">Veruca's Midnight Spacemen</h3>
-          <p className="venue-name">Venue: Pompeii ruins</p>
-          <p>Date:04/20/2004</p>
-
-          {star}
-          {star}
-          {star}
-          {star}
-          {star}
-          <h4 className="tagline">
-            This is a user tagline lorem ipsum dolor sit amet
-          </h4>
-          <p className="user-name">
-            <span className="user-thumbnail">{userThumbnail}</span> Username{" "}
-            <span className="user-status">(avid concertgoer)</span>
-          </p>
-          <div className="review">
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris a
-              orci tincidunt, rhoncus ipsum in, tincidunt est. Fusce aliquam
-              quam nec vulputate varius. In hac habitasse platea dictumst.
-              Vivamus vulputate rhoncus ipsum sollicitudin maximus. Pellentesque
-              id risus efficitur, egestas odio sit amet, ege
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="push" />
-    </div>
-  );
+      </>
+    );
+  }
 }
-
 export default BrowseReviewsPage;
