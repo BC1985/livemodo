7f8063bc3d1c58f0835f6fee52235907902203e4
Ben Cohen <benjcohen1985@gmail.com>
2019-07-09 13:02:57 -0400

    Add preliminary youtube streaming functionality

--- src/BrowseForm.js
+++ src/BrowseForm.js
@@ -1,17 +1,26 @@
 import React, { Component } from "react";
 import Button from "./Button";
 import "./BrowseForm.css";
-
+import { API_KEY } from "./config";
 class BrowseForm extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+  handleSubmit = e => {
+    e.preventDefault();
+    const url = `https://www.googleapis.com/youtube/v3/videos?id=w_7_lEOaU10&key=${API_KEY}&part=snippet`;
+    fetch(url)
+      .then(res => {
+        res.json();
+      })
+      .then(res => console.log(res));
+  };
   render() {
     return (
       <div className="search-form">
-        <form id="search-form">
-          <input type="text" placeholder="Search performer or venue " />
+        <form id="search-form" onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Search performer or venue" />
           <Button>search</Button>
         </form>
       </div>
---
+++ src/Youtube.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import {API_KEY} from './config'
+
+class Youtube extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <div className="youtube" />
+      </div>
+    );
+  }
+}
+
+export default Youtube;
