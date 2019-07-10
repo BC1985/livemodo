7c4fd13d8674a82c56e9308556a195c3645e051d
Ben Cohen <benjcohen1985@gmail.com>
2019-07-09 20:36:25 -0400

    Clean up code

--- src/Review.js
+++ src/Review.js
@@ -6,31 +6,24 @@ import "./BrowseReviewsPage.css";
 import { parseDate } from "./utils/parseDate";
 
 class Review extends Component {
-  state = {
-    reviews: []
-  };
-
   render() {
     const star = <FontAwesomeIcon icon={faStar} />;
     const userThumbnail = <FontAwesomeIcon icon={faPortrait} />;
     const review = this.props;
     return (
-      <div className="review-container">
-        <div key={review.id}>
-          <h2 className="band-name">{review.band_name}</h2>
-          <p>Venue: {review.venue}</p>
-          <p>Date:{parseDate(review.show_date)}</p>
-          {star}
-          <h4 className="tagline">{review.tagline}</h4>
-          <p>posted: {parseDate(review.posted)}</p>
-
-          <p className="user-name">
-            <span className="user-thumbnail">{userThumbnail}</span> Username{" "}
-            <span className="user-status">(avid concertgoer)</span>
-          </p>
-          <div className="review">{review.content}</div>
-        </div>
-      </div>
+      <>
+        <h2 className="band-name">{review.band_name}</h2>
+        <p>Venue: {review.venue}</p>
+        <p>Date:{parseDate(review.show_date)}</p>
+        {star}
+        <h4 className="tagline">{review.tagline}</h4>
+        <p>posted: {parseDate(review.posted)}</p>
+        <p className="user-name">
+          <span className="user-thumbnail">{userThumbnail}</span> Username{" "}
+          <span className="user-status">(avid concertgoer)</span>
+        </p>
+        <div className="review">{review.content}</div>
+      </>
     );
   }
 }
