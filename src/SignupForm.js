61fcd1f7f63816134d6a2821a87db94eec669eb3
Ben Cohen <benjcohen1985@gmail.com>
2019-07-13 11:23:47 -0400

    Add input props and backend connectivity to form

--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
 import "./SignupForm.css";
 import Button from "./Button";
+import AuthApiService from "./services/auth-api-service";
 
 class SignupForm extends Component {
+  state = {
+    error: null,
+    first_name: "",
+    last_name: "",
+    username: "",
+    password: "",
+    email: ""
+  };
+
+  onChange = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+    console.log(this.state);
+  };
   handleSubmit = e => {
     e.preventDefault();
+    const { first_name, last_name, username, password } = e.target;
+    this.setState({ error: null });
+    AuthApiService.postUser({
+      first_name: first_name.value,
+      last_name: last_name.value,
+      username: username.value,
+      password: password.value
+    })
+      .then(user => {
+        first_name.value = "";
+        last_name.value = "";
+        username.value = "";
+        password.value = "";
+      })
+      .catch(res => this.setState({ error: res.error }));
+
     this.props.changeState();
   };
   render() {
@@ -13,6 +47,7 @@ class SignupForm extends Component {
       fontStyle: "italic",
       fontSize: "12px"
     };
+    console.log(this.state);
 
     return (
       <>
@@ -23,23 +58,56 @@ class SignupForm extends Component {
           <form id="signup" onSubmit={this.handleSubmit}>
             <div className="text-input">
               <label>*First Name </label>
-              <input type="text" placeholder="e.g Tommy" required />
+              <input
+                type="text"
+                name="first_name"
+                placeholder="e.g Tommy"
+                value={this.state.first_name}
+                onChange={this.onChange}
+                required
+              />
             </div>
             <div className="text-input">
               <label>*Last Name </label>
-              <input type="text" placeholder="e.g Wisseau" required />
+              <input
+                type="text"
+                name="last_name"
+                placeholder="e.g Wisseau"
+                value={this.state.last_name}
+                onChange={this.onChange}
+                required
+              />
             </div>
             <div className="text-input">
               <label>*Username </label>
-              <input type="text" required />
+              <input
+                type="text"
+                name="username"
+                value={this.state.username}
+                onChange={this.onChange}
+                required
+              />
             </div>
             <div className="text-input">
               <label>*Email </label>
-              <input type="text" placeholder="e.g tommy@theroom.com" required />
+              <input
+                type="text"
+                name="email"
+                placeholder="e.g tommy@theroom.com"
+                value={this.state.email}
+                onChange={this.onChange}
+                required
+              />
             </div>
             <div className="text-input">
               <label>*Password</label>
-              <input type="text" required />
+              <input
+                type="text"
+                name="password"
+                value={this.state.password.value}
+                onChange={this.onChange}
+                required
+              />
             </div>
 
             <p style={spanStyle}>(* indicates required field)</p>
