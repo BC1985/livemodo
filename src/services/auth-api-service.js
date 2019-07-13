b3c9cfdc7b0cac431205b67546d73f148becfeb8
Ben Cohen <benjcohen1985@gmail.com>
2019-07-13 11:18:59 -0400

    Add post user functionality to backend

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -12,7 +12,22 @@ const AuthApiService = {
       if (!res.ok) {
         res.json().then(e => Promise.reject(e));
       } else {
-        res.josn();
+        res.json();
+      }
+    });
+  },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/users`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(user)
+    }).then(res => {
+      if (!res.ok) {
+        res.json().then(e => Promise.reject(e));
+      } else {
+        res.json();
       }
     });
   }
