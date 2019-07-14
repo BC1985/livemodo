cf5cb082af722cb6c9db369b3e24ca51fd589b6b
Ben Cohen <benjcohen1985@gmail.com>
2019-07-14 16:21:55 -0400

    Add token function

--- src/utils/token-service.js
+++ src/utils/token-service.js
@@ -3,9 +3,14 @@ import config from "../config";
 export const TokenService = {
   saveAuthToken(token) {
     window.localStorage.setItem(config.API_TOKEN, token);
+    // console.log("token", token);
+    // console.log("localStorage", window.localStorage.getItem(config.API_TOKEN));
+  },
+  makeBasicAuthToken(username, password) {
+    return window.btoa(`${username}:${password}`);
   },
 
   getAuthToken(token) {
-    window.localStorage.getItem(config.API_TOKEN);
+    return window.localStorage.getItem(config.API_TOKEN);
   }
 };
