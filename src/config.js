fa723013ca5597277fd4abba1ede507d97b442ef
Ben Cohen <benjcohen1985@gmail.com>
2019-07-14 16:21:24 -0400

    Change token

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 module.exports = {
   API_KEY: process.env.YOUTUBE_API_KEY,
-  API_TOKEN: process.env.API_TOKEN,
+  API_TOKEN: "secret-token-very-hush-hush",
   API_ENDPOINT: "http://localhost:8000/api"
 };
