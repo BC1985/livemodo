9b8abef2921be8b86e51e9d46a5e03c6c5891724
Ben Cohen <benjcohen1985@gmail.com>
2019-07-15 14:53:30 -0400

    Save token in session storage

--- src/utils/token-service.js
+++ src/utils/token-service.js
@@ -2,21 +2,22 @@ import config from "../config";
 
 export const TokenService = {
   saveAuthToken(token) {
-    window.localStorage.setItem(config.API_TOKEN, token);
+    window.sessionStorage.setItem(config.API_TOKEN, token);
     // console.log("token", token);
-    // console.log("localStorage", window.localStorage.getItem(config.API_TOKEN));
+    // console.log("sessionStorage", window.sessionStorage.getItem(config.API_TOKEN));
   },
   makeBasicAuthToken(username, password) {
     return window.btoa(`${username}:${password}`);
   },
   clearAuthToken() {
-    window.localStorage.removeItem(config.API_TOKEN);
+    window.sessionStorage.removeItem(config.API_TOKEN);
+    console.info("clearing auth token");
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken();
   },
 
   getAuthToken(token) {
-    return window.localStorage.getItem(config.API_TOKEN);
+    return window.sessionStorage.getItem(config.API_TOKEN);
   }
 };
