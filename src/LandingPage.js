f0bf8449e03e5a89886151d0b39d23f5ca5446cd
Ben Cohen <benjcohen1985@gmail.com>
2019-07-15 14:53:07 -0400

    Add conditional rendering for log in button

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Redirect, Link } from "react-router-dom";
 import Button from "./Button";
 import "./LandingPage.css";
+import { TokenService } from "./utils/token-service";
 
 // import LoginForm from "./LoginPage";
 const Description = () => {
@@ -30,7 +31,6 @@ export default class LandingPage extends Component {
     this.setState({
       Redirect: true
     });
-    console.log(this.state);
   };
   UserNotLoggedIn = () => {
     return (
@@ -41,39 +41,24 @@ export default class LandingPage extends Component {
       </Link>
     );
   };
-
-  // isLoggedIn = () => {
-  //   if (!this.props.isLoggedIn) {
-  //     return (
-  //       <Link to="/login">
-  //         <Button>Log in</Button>
-  //       </Link>
-  //     );
-  //   }
-  // };
-  // isNewUser = () => {
-  //   return (
-  //     <Link to="/register">
-  //       <Button>Register</Button>
-  //     </Link>
-  //   );
-  // };
-
   render() {
-    const { isLoggedIn } = this.props;
-
     if (this.state.Redirect) {
       return <Redirect push to="/login" />;
     }
-    const logIn = <this.UserNotLoggedIn />;
     return (
       <div className="container">
         <div className="header">
           <h1>Livemodo</h1>
           <h3>Your wiki live shows resource</h3>
         </div>
-        <div id={!isLoggedIn ? "login" : "greeting"}>
-          {!isLoggedIn ? logIn : null}
+        <div>
+          {TokenService.hasAuthToken() ? (
+            <div id="welcome">Welcome!</div>
+          ) : (
+            <Link to="/login">
+              <Button>Login</Button>
+            </Link>
+          )}
         </div>
 
         <div className="description">
