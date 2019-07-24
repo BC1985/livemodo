70f9a30d1aa961d4686aaf93ebeeaa053d2870e9
Ben Cohen <benjcohen1985@gmail.com>
2019-07-24 15:54:20 -0400

    Change error message

--- src/ErrorMessage.js
+++ src/ErrorMessage.js
@@ -1,9 +1,10 @@
 import React from "react";
 const errorStyle = {
+  textAlign: "center",
   color: "red"
 };
 function ErrorMessage() {
-  return <div style={errorStyle}>Error</div>;
+  return <div style={errorStyle}>Something went wrong, please try again.</div>;
 }
 
 export default ErrorMessage;
