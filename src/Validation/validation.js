f495e13c2ccfd57267e934d9830e7ab16a142330
Ben Cohen <benjcohen1985@gmail.com>
2019-07-26 14:03:29 -0400

    Add registration form validation function

--- src/Validation/validation.js
+++ src/Validation/validation.js
@@ -6,6 +6,22 @@ export function validateAddReview(venue, band_name, show_date, rating) {
     rating: rating.length === 0
   };
 }
+
+export function validateRegisration(
+  first_name,
+  last_name,
+  username,
+  password,
+  email
+) {
+  return {
+    first_name: first_name.length === 0,
+    last_name: last_name.length === 0,
+    username: username.length === 0,
+    password: password.length === 0,
+    email: email.length === 0
+  };
+}
 export const shouldBeError = (field, errors, touched) => {
   const hasErrors = errors[field];
   const shouldShow = touched[field];
