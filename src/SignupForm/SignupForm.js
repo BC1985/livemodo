5bd9689c937cdb291161c4a7535a608244f578a8
Ben Cohen <benjcohen1985@gmail.com>
2019-07-26 14:06:31 -0400

    Add form validation an button rendering

--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import "./SignupForm.css";
-import Button from "../Button/Button";
 import AuthApiService from "../services/auth-api-service";
 import { withRouter } from "react-router-dom";
+import { validateRegisration, shouldBeError } from "../Validation/validation";
 
 class SignupForm extends Component {
   state = {
@@ -11,12 +11,27 @@ class SignupForm extends Component {
     last_name: "",
     username: "",
     password: "",
-    email: ""
+    email: "",
+    errors: {
+      first_name: false,
+      last_name: false,
+      username: false,
+      password: false,
+      email: false
+    },
+    touched: {
+      first_name: false,
+      last_name: false,
+      username: false,
+      password: false,
+      email: false
+    },
+    errorMessage: false
   };
 
   onChange = e => {
     const name = e.target.name;
-    const value = e.target.type === "radio" ? e.target.checked : e.target.value;
+    const value = e.target.value;
     this.setState({
       [name]: value
     });
@@ -46,12 +61,35 @@ class SignupForm extends Component {
 
     this.props.history.push("thank-you");
   };
+  handleBlur = field => e => {
+    this.setState({
+      touched: { ...this.state.touched, [field]: true },
+      ErrorMessage: true
+    });
+  };
+
   render() {
     const spanStyle = {
       color: "whiteSmoke",
       fontStyle: "italic",
       fontSize: "0.8em"
     };
+    const {
+      first_name,
+      last_name,
+      username,
+      password,
+      email,
+      touched
+    } = this.state;
+    const errors = validateRegisration(
+      first_name,
+      last_name,
+      username,
+      password,
+      email
+    );
+    const isEnabled = !Object.keys(errors).some(x => errors[x]);
 
     return (
       <div className="signup-container">
@@ -63,10 +101,14 @@ class SignupForm extends Component {
             <div className="text-input">
               <label>*First Name </label>
               <input
+                className={
+                  shouldBeError("first_name", errors, touched) ? "error" : null
+                }
                 type="text"
                 name="first_name"
                 placeholder="e.g Tommy"
-                value={this.state.first_name}
+                value={first_name}
+                onBlur={this.handleBlur("first_name", errors, touched)}
                 onChange={this.onChange}
                 required
               />
@@ -74,10 +116,14 @@ class SignupForm extends Component {
             <div className="text-input">
               <label>*Last Name </label>
               <input
+                className={
+                  shouldBeError("last_name", errors, touched) ? "error" : null
+                }
                 type="text"
                 name="last_name"
                 placeholder="e.g Wisseau"
-                value={this.state.last_name}
+                value={last_name}
+                onBlur={this.handleBlur("last_name", errors, touched)}
                 onChange={this.onChange}
                 required
               />
@@ -85,9 +131,13 @@ class SignupForm extends Component {
             <div className="text-input">
               <label>*Username </label>
               <input
+                className={
+                  shouldBeError("username", errors, touched) ? "error" : null
+                }
                 type="text"
                 name="username"
-                value={this.state.username}
+                value={username}
+                onBlur={this.handleBlur("username", errors, touched)}
                 onChange={this.onChange}
                 required
               />
@@ -95,10 +145,14 @@ class SignupForm extends Component {
             <div className="text-input">
               <label>*Email </label>
               <input
+                className={
+                  shouldBeError("email", errors, touched) ? "error" : null
+                }
                 type="text"
                 name="email"
                 placeholder="e.g tommy@theroom.com"
-                value={this.state.email}
+                value={email}
+                onBlur={this.handleBlur("email", errors, touched)}
                 onChange={this.onChange}
                 required
               />
@@ -106,9 +160,13 @@ class SignupForm extends Component {
             <div className="text-input">
               <label>*Password </label>
               <input
+                className={
+                  shouldBeError("password", errors, touched) ? "error" : null
+                }
                 type="text"
                 name="password"
-                value={this.state.password}
+                value={password}
+                onBlur={this.handleBlur("password", errors, touched)}
                 onChange={this.onChange}
                 required
               />
@@ -116,7 +174,13 @@ class SignupForm extends Component {
 
             <p style={spanStyle}>* indicates required field</p>
 
-            <Button type="submit">Submit</Button>
+            <button
+              id="register-button"
+              type={!isEnabled ? "disabled" : "submit"}
+              disabled={!isEnabled}
+            >
+              Submit
+            </button>
           </form>
           <div className="register-push" />
         </div>
