f34622c06a52756e6763af3dea2ba8c1f6c6efbf
Ben Cohen <benjcohen1985@gmail.com>
2019-10-21 15:32:16 -0400

    Change to class and style based on props

--- src/DrawerToggleBottun/DrawerToggleButon.js
+++ src/DrawerToggleBottun/DrawerToggleButon.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./DrawerToggleButton.css";
 
-const drawerToggleButton = props => (
-  <button className="toggle_button" onClick={props.click}>
-    <div className="toggle_button_line" />
-    <div className="toggle_button_line" />
-    <div className="toggle_button_line" />
-  </button>
-);
+class drawerToggleButton extends Component {
+  render() {
+    const { click, isSideDrawerOpen } = this.props;
+    const styles = {
+      line: {
+        height: "3px",
+        width: "20px",
+        background: "white",
+        transition: "all 0.3s ease",
+        zIndex: 10
+      },
+      lineTop: {
+        transform: isSideDrawerOpen ? "rotate(50deg)" : "none",
+        transformOrigin: "top left",
+        marginBottom: "5px"
+      },
+      lineMiddle: {
+        opacity: isSideDrawerOpen ? 0 : 1,
+        transform: isSideDrawerOpen ? "translateX(-16px)" : "none"
+      },
+      lineBottom: {
+        transform: isSideDrawerOpen
+          ? "translateX(-1px) rotate(-50deg)"
+          : "none",
+        transformOrigin: "top left",
+        marginTop: "5px"
+      }
+    };
+    return (
+      <button id="toggle-button" onClick={click}>
+        <div style={{ ...styles.line, ...styles.lineTop }} />
+        <div style={{ ...styles.line, ...styles.lineMiddle }} />
+        <div style={{ ...styles.line, ...styles.lineBottom }} />
+      </button>
+    );
+  }
+}
 
 export default drawerToggleButton;
