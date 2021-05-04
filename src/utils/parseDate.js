5066e86f783a4d9426ccfc206e9035967d71d878
Ben Cohen <benjcohen1985@gmail.com>
2021-05-04 21:02:54 +0300

    Include timestamp

--- src/utils/parseDate.js
+++ src/utils/parseDate.js
@@ -1,7 +1,16 @@
-export const parseDate = strDate => {
+export const postedAt = strDate => {
   const date = new Date(strDate);
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  return `${month}/${day}/${year}`;
+  const time = `${date.getHours()}: ${date.getMinutes()}`;
+  return `${month}/${day}/${year} at ${time}`;
 };
+
+export const showDate = str =>{
+  const date = new Date(str);
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  return `${month}/${day}/${year}`;
+}
