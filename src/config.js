1ef14d637a5c52a597414dc2709b85b36e54e5f2
Ben Cohen <benjcohen1985@gmail.com>
2021-05-04 22:51:44 +0300

    Replace connection url

--- src/config.js
+++ src/config.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 module.exports = {
-  API_KEY: process.env.YOUTUBE_API_KEY,
-  API_ENDPOINT: "http://localhost:8000/api",
   API_BASE_URL:
-    // process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:5000"
+    "https://livemodo.herokuapp.com"
 };
