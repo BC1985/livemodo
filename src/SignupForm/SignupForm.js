2e665022826e5df4b4d2fa2a13480b497acb8b03
Ben Cohen <benjcohen1985@gmail.com>
2021-05-05 19:14:32 +0300

    Redo form using MaterialUI

--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -1,11 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 import "./SignupForm.css";
 import AuthApiService from "../services/auth-api-service";
 import { withRouter } from "react-router-dom";
-import { validateRegisration, shouldBeError } from "../Validation/validation";
-
-
+// import { inputIsEmpty, shouldBeError } from "../Validation/validation";
+import { makeStyles } from "@material-ui/core/styles";
 import { useState } from "react";
+import { TextField, Button, Container } from "@material-ui/core";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
 
 function SignupForm(props) {
   const [input, setInput] = useState({
@@ -14,14 +16,23 @@ function SignupForm(props) {
     username: "",
     email: "",
     password: "",
+    repeatPassword: "",
   });
-  const { email, password, first_name, last_name, username } = input;
+  const {
+    email,
+    password,
+    first_name,
+    last_name,
+    username,
+    repeatPassword,
+  } = input;
   const [touched, setTouched] = useState({
     first_name: false,
     last_name: false,
     username: false,
     email: false,
     password: false,
+    repeatPassword: false,
   });
   const [errors, setErrors] = useState({
     first_name: false,
@@ -29,13 +40,13 @@ function SignupForm(props) {
     username: false,
     email: false,
     password: false,
+    repeatPassword: false,
   });
-  const [passwordError, setPasswordError] = useState(false)
-  const [hasError, setHasError] = useState(false)
-  const [error, setError] = useState("")
+  const [passwordError, setPasswordError] = useState(false);
+  const [hasError, setHasError] = useState(false);
+  // const [errors, setErrors] = useState({});
   // state = {
   //   error: null,
-
 
   //   errorMessage: false,
   //   passwordError: false
@@ -47,7 +58,6 @@ function SignupForm(props) {
   };
 
   const validatePassword = () => {
-    
     const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/;
     if (password.length < 8) {
       return "Password must be at least 8 characters";
@@ -65,38 +75,34 @@ function SignupForm(props) {
   };
 
   const renderError = e => {
-    setPasswordError(true)
+    setPasswordError(true);
   };
 
-  const handleSubmit = e => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const passwordNotValid = validatePassword();
+    try {
+      const passwordNotValid = validatePassword();
 
-    if (passwordNotValid) {
-      // this.setState({ error: true }, renderError());
-      setHasError(true)
-      renderError()
-    } else {
-      // const { first_name, last_name, email, username, password } = e.target;
-      AuthApiService.postUser({
-        first_name,
-        last_name,
-        username,
-        password,
-        email
-      })
-        .then(user => {
-          first_name.value = "";
-          last_name.value = "";
-          username.value = "";
-          password.value = "";
-          email.value = "";
-        })
-        .catch(console.log(res));
-
-      props.history.push("thank-you");
+      if (passwordNotValid) {
+        setHasError(true);
+        renderError();
+      } else {
+        AuthApiService.postUser({
+          first_name,
+          last_name,
+          username,
+          password,
+          email,
+        });
+        setInput(input => ({ ...(input === "") }));
+        // props.history.push("thank-you");
+      }
+    } catch (error) {
+      console.log(error);
     }
   };
+
+
   const handleBlur = field => e => {
     setTouched({ ...touched, [field]: true });
   };
@@ -109,115 +115,337 @@ function SignupForm(props) {
   const ifPasswordError = {
     border: "2px solid red",
   };
-
-  const validationErrors = validateRegisration(
+  function inputIsEmpty(
     first_name,
     last_name,
     username,
     password,
+    email,
+    repeatPassword
+  ) {
+    return {
+      first_name: first_name.length === 0,
+      last_name: last_name.length === 0,
+      username: username.length === 0,
+      password: password.length === 0,
+      repeatPassword: repeatPassword.length === 0,
+      email: email.length === 0
+    };
+  }
+  const validationErrors = inputIsEmpty(
+    first_name,
+    last_name,
+    username,
+    password,
+    repeatPassword,
     email
   );
-  const isEnabled = !Object.keys(validationErrors).some(x => errors[x]);
   
-  const errorMessage = validatePassword();
-  return (
-    <div className="signup-container">
-      <div className="call-to-action">
-        <h2>Join the Livemodo community!</h2>
-      </div>
-      <div className="signup-form">
-        <form id="signup" onSubmit={handleSubmit}>
-          <div className="text-input">
-            <label>*First Name </label>
-            <input
-              className={
-                shouldBeError("first_name", validationErrors, touched) ? "error" : null
-              }
-              type="text"
-              name="first_name"
-              placeholder="e.g Tommy"
-              value={first_name}
-              onBlur={handleBlur("first_name", validationErrors, touched)}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="text-input">
-            <label>*Last Name </label>
-            <input
-              className={
-                shouldBeError("last_name", validationErrors, touched) ? "error" : null
-              }
-              type="text"
-              name="last_name"
-              placeholder="e.g Wisseau"
-              value={last_name}
-              onBlur={handleBlur("last_name", validationErrors, touched)}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="text-input">
-            <label>*Username </label>
-            <input
-              className={
-                shouldBeError("username", validationErrors, touched) ? "error" : null
-              }
-              type="text"
-              name="username"
-              value={username}
-              onBlur={handleBlur("username", validationErrors, touched)}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="text-input">
-            <label>*Email </label>
-            <input
-              className={
-                shouldBeError("email", validationErrors, touched) ? "error" : null
-              }
-              type="text"
-              name="email"
-              placeholder="e.g tommy@theroom.com"
-              value={email}
-              onBlur={handleBlur("email", validationErrors, touched)}
-              onChange={onChange}
-              required
-            />
-          </div>
-          <div className="text-input">
-            <label>*Password </label>
-            <input
-              className={
-                shouldBeError("password", validationErrors, touched) ? "error" : null
-              }
-              type="text"
-              name="password"
-              value={password}
-              onBlur={handleBlur("password", validationErrors, touched)}
-              onChange={onChange}
-              required
-              style={passwordError ? ifPasswordError : null}
-            />
-            <span className="errorMessage">
-              {error ? errorMessage : ""}
-            </span>
-          </div>
-
-          <p style={spanStyle}>* indicates required field</p>
-          <button
-            id="register-button"
-            type={!isEnabled ? "disabled" : "submit"}
-            disabled={!isEnabled}
-          >
-            Submit
-          </button>
-        </form>
-        <div className="register-push" />
-      </div>
-    </div>
+  const isEnabled = !Object.keys(validationErrors).some(
+    x => validationErrors[x]
   );
+
+  const errorMessage = validatePassword();
+  // const useStyles = makeStyles({
+  //   root: {
+  //     minWidth: 275,
+  //   },
+  //   bullet: {
+  //     display: 'inline-block',
+  //     margin: '0 2px',
+  //     transform: 'scale(0.8)',
+  //   },
+  //   title: {
+  //     fontSize: 14,
+  //   },
+  //   pos: {
+  //     marginBottom: 12,
+  //   },
+  // });
+  // const classes = useStyles();
+  const useStyles = makeStyles(theme => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      marginBottom: 200,
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+    },
+    form: {
+      width: "100%",
+      marginTop: theme.spacing(8),
+      marginBottom: theme.spacing(8),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+  const classes = useStyles();
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <div className={classes.paper}>
+        <Typography component="h1" variant="h5">
+          Join the Livemodo community
+        </Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                name="first_name"
+                variant="outlined"
+                value={first_name}
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                value={last_name}
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="last_name"
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                type="email"
+                value={email}
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                value={username}
+                fullWidth
+                id="username"
+                label="Username"
+                name="username"
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                value={password}
+                fullWidth
+                name="password"
+                helperText="Must contain uppercase, lowercase, numbers and special characters"
+                label="Password"
+                type="text"
+                id="password"
+                onChange={onChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                value={repeatPassword}
+                required
+                fullWidth
+                name="repeatPassword"
+                label="Repeat Password"
+                type="password"
+                id="passwordRepeat"
+                onChange={onChange}
+              />
+            </Grid>
+          </Grid>
+          <Button
+            disabled={!isEnabled && true}
+            type="submit"
+            value={first_name}
+            fullWidth
+            variant="contained"
+            color="default"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+  // return (
+  //   // <Container>
+  //   <div className="signup-container">
+  //     <div className="call-to-action">
+  //       <h2>Join the Livemodo community</h2>
+  //     </div>
+  //     {/* <Container> */}
+  //     <div className="signup-form">
+  //       <Card>
+  //         <CardContent>
+  //           <FormGroup>
+  //             <form className={classes.root}>
+  //             {/* <div> */}
+  //               {/* <label>*First Name </label> */}
+  //               {/* <TextField
+  //             className={
+  //               shouldBeError("first_name", validationErrors, touched) ? "error" : null
+  //             }
+  //             type="text"
+  //             name="first_name"
+  //             placeholder="e.g Tommy"
+  //             value={first_name}
+  //             onBlur={handleBlur("first_name", validationErrors, touched)}
+  //             onChange={onChange}
+  //             required
+  //           /> */}
+  //               <TextField
+  //                 id="outlined-basic"
+  //                 label="First name"
+  //                 variant="outlined"
+  //                 name="first_name"
+  //                 required
+  //                 onChange={onChange}
+  //                 value={first_name}
+  //               />
+  //             {/* </div> */}
+  //             {/* <div> */}
+  //               <TextField
+  //                 id="outlined-basic"
+  //                 variant="outlined"
+  //                 label="Last name"
+  //                 name="last_name"
+  //                 required
+  //                 onChange={onChange}
+  //                 value={last_name}
+  //               />
+  //               {/* <label>*Last Name </label> */}
+  //               {/* <TextField
+  //             className={
+  //               shouldBeError("last_name", validationErrors, touched) ? "error" : null
+  //             }
+  //             type="text"
+  //             name="last_name"
+  //             placeholder="e.g Wisseau"
+  //             value={last_name}
+  //             onBlur={handleBlur("last_name", validationErrors, touched)}
+  //             onChange={onChange}
+  //             required
+  //           /> */}
+  //             {/* </div> */}
+  //             {/* <div> */}
+  //               {/* <label>*Username </label> */}
+  //               <TextField
+  //                 id="outlined-basic"
+  //                 label="username"
+  //                 variant="outlined"
+  //                 name="username"
+  //                 required
+  //                 onChange={onChange}
+  //                 value={username}
+  //               />
+  //               {/* <TextField
+  //             className={
+  //               shouldBeError("username", validationErrors, touched) ? "error" : null
+  //             }
+  //             type="text"
+  //             name="username"
+  //             value={username}
+  //             onBlur={handleBlur("username", validationErrors, touched)}
+  //             onChange={onChange}
+  //             required
+  //           /> */}
+  //             {/* </div> */}
+  //             {/* <div> */}
+  //               {/* <label>*Email </label> */}
+  //               <TextField
+  //                 id="outlined-basic"
+  //                 variant="outlined"
+  //                 label="email"
+  //                 name="email"
+  //                 required
+  //                 onChange={onChange}
+  //                 value={email}
+  //               />
+  //               {/* <TextField
+  //             className={
+  //               shouldBeError("email", validationErrors, touched) ? "error" : null
+  //             }
+  //             type="text"
+  //             name="email"
+  //             placeholder="e.g tommy@theroom.com"
+  //             value={email}
+  //             onBlur={handleBlur("email", validationErrors, touched)}
+  //             onChange={onChange}
+  //             required
+  //           /> */}
+  //             {/* </div> */}
+  //             {/* <div> */}
+  //               {/* <label>*Password </label> */}
+  //               <TextField
+  //                 id="outlined-basic"
+  //                 variant="outlined"
+  //                 label="Password"
+  //                 name="password"
+  //                 required
+  //                 onChange={onChange}
+  //                 value={password}
+  //               />
+  //               {/* <TextField
+  //             className={
+  //               shouldBeError("password", validationErrors, touched) ? "error" : null
+  //             }
+  //             type="text"
+  //             name="password"
+  //             value={password}
+  //             onBlur={handleBlur("password", validationErrors, touched)}
+  //             onChange={onChange}
+  //             required
+  //             style={passwordError ? ifPasswordError : null}
+  //           /> */}
+  //               <span className="errorMessage">
+  //                 {error ? errorMessage : ""}
+  //               </span>
+  //             {/* </div> */}
+
+  //             <p style={spanStyle}>* indicates required field</p>
+  //             <Button
+  //               variant="contained"
+  //               // id="register-button"
+  //               // type={!isEnabled ? "disabled" : "submit"}
+  //               // disabled={!isEnabled}
+  //             >
+  //               Sign up
+  //             </Button>
+  //             {/* <button
+  //           id="register-button"
+  //           type={!isEnabled ? "disabled" : "submit"}
+  //           disabled={!isEnabled}
+  //         >
+  //           Submit
+  //         </button> */}
+  //             </form>
+  //           </FormGroup>
+  //         </CardContent>
+  //       </Card>
+  //       <div className="register-push" />
+  //     </div>
+
+  {
+    /* </Container> */
+  }
+  // </div>
+  // );
 }
 
 export default withRouter(SignupForm);
--- src/Validation/validation.js
+++ src/Validation/validation.js
@@ -7,18 +7,20 @@ export function validateAddReview(venue, band_name, show_date, rating) {
   };
 }
 
-export function validateRegisration(
+export function inputIsEmpty(
   first_name,
   last_name,
   username,
   password,
-  email
+  email,
+  repeatPassword
 ) {
   return {
     first_name: first_name.length === 0,
     last_name: last_name.length === 0,
     username: username.length === 0,
     password: password.length === 0,
+    repeatPassword: repeatPassword.length === 0,
     email: email.length === 0
   };
 }
