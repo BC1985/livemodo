2b1a7f44820dbbe1a93e2fc58f281ceb6d652185
Ben Cohen <benjcohen1985@gmail.com>
2021-05-05 11:13:19 +0300

    Refractor code

--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -4,42 +4,50 @@ import AuthApiService from "../services/auth-api-service";
 import { withRouter } from "react-router-dom";
 import { validateRegisration, shouldBeError } from "../Validation/validation";
 
-class SignupForm extends Component {
-  state = {
-    error: null,
+
+import { useState } from "react";
+
+function SignupForm(props) {
+  const [input, setInput] = useState({
     first_name: "",
     last_name: "",
     username: "",
-    password: "",
     email: "",
-    errors: {
-      first_name: false,
-      last_name: false,
-      username: false,
-      password: false,
-      email: false
-    },
-    touched: {
-      first_name: false,
-      last_name: false,
-      username: false,
-      password: false,
-      email: false
-    },
-    errorMessage: false,
-    passwordError: false
+    password: "",
+  });
+  const { email, password, first_name, last_name, username } = input;
+  const [touched, setTouched] = useState({
+    first_name: false,
+    last_name: false,
+    username: false,
+    email: false,
+    password: false,
+  });
+  const [errors, setErrors] = useState({
+    first_name: false,
+    last_name: false,
+    username: false,
+    email: false,
+    password: false,
+  });
+  const [passwordError, setPasswordError] = useState(false)
+  const [hasError, setHasError] = useState(false)
+  const [error, setError] = useState("")
+  // state = {
+  //   error: null,
+
+
+  //   errorMessage: false,
+  //   passwordError: false
+  // };
+
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInput(input => ({ ...input, [name]: value }));
   };
 
-  onChange = e => {
-    const name = e.target.name;
-    const value = e.target.value;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  validatePassword = () => {
-    const { password } = this.state;
+  const validatePassword = () => {
+    
     const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/;
     if (password.length < 8) {
       return "Password must be at least 8 characters";
@@ -56,26 +64,26 @@ class SignupForm extends Component {
     return null;
   };
 
-  renderError = e => {
-    this.setState({
-      passwordError: true
-    });
+  const renderError = e => {
+    setPasswordError(true)
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    const passwordNotValid = this.validatePassword();
+    const passwordNotValid = validatePassword();
 
     if (passwordNotValid) {
-      this.setState({ error: true }, this.renderError());
+      // this.setState({ error: true }, renderError());
+      setHasError(true)
+      renderError()
     } else {
-      const { first_name, last_name, email, username, password } = e.target;
+      // const { first_name, last_name, email, username, password } = e.target;
       AuthApiService.postUser({
-        first_name: first_name.value,
-        last_name: last_name.value,
-        username: username.value,
-        password: password.value,
-        email: email.value
+        first_name,
+        last_name,
+        username,
+        password,
+        email
       })
         .then(user => {
           first_name.value = "";
@@ -84,145 +92,132 @@ class SignupForm extends Component {
           password.value = "";
           email.value = "";
         })
-        .catch(res => {
-          this.setState({ error: res.error });
-        });
+        .catch(console.log(res));
 
-      this.props.history.push("thank-you");
+      props.history.push("thank-you");
     }
   };
-  handleBlur = field => e => {
-    this.setState({
-      touched: { ...this.state.touched, [field]: true }
-    });
+  const handleBlur = field => e => {
+    setTouched({ ...touched, [field]: true });
   };
 
-  render() {
-    const spanStyle = {
-      color: "whiteSmoke",
-      fontStyle: "italic",
-      fontSize: "0.8em"
-    };
-    const ifPasswordError = {
-      border: "2px solid red"
-    };
-    const {
-      first_name,
-      last_name,
-      username,
-      password,
-      email,
-      touched
-    } = this.state;
-    const errors = validateRegisration(
-      first_name,
-      last_name,
-      username,
-      password,
-      email
-    );
-    const isEnabled = !Object.keys(errors).some(x => errors[x]);
-    const errorMessage = this.validatePassword();
-    return (
-      <div className="signup-container">
-        <div className="call-to-action">
-          <h2>Join the Livemodo community!</h2>
-        </div>
-        <div className="signup-form">
-          <form id="signup" onSubmit={this.handleSubmit}>
-            <div className="text-input">
-              <label>*First Name </label>
-              <input
-                className={
-                  shouldBeError("first_name", errors, touched) ? "error" : null
-                }
-                type="text"
-                name="first_name"
-                placeholder="e.g Tommy"
-                value={first_name}
-                onBlur={this.handleBlur("first_name", errors, touched)}
-                onChange={this.onChange}
-                required
-              />
-            </div>
-            <div className="text-input">
-              <label>*Last Name </label>
-              <input
-                className={
-                  shouldBeError("last_name", errors, touched) ? "error" : null
-                }
-                type="text"
-                name="last_name"
-                placeholder="e.g Wisseau"
-                value={last_name}
-                onBlur={this.handleBlur("last_name", errors, touched)}
-                onChange={this.onChange}
-                required
-              />
-            </div>
-            <div className="text-input">
-              <label>*Username </label>
-              <input
-                className={
-                  shouldBeError("username", errors, touched) ? "error" : null
-                }
-                type="text"
-                name="username"
-                value={username}
-                onBlur={this.handleBlur("username", errors, touched)}
-                onChange={this.onChange}
-                required
-              />
-            </div>
-            <div className="text-input">
-              <label>*Email </label>
-              <input
-                className={
-                  shouldBeError("email", errors, touched) ? "error" : null
-                }
-                type="text"
-                name="email"
-                placeholder="e.g tommy@theroom.com"
-                value={email}
-                onBlur={this.handleBlur("email", errors, touched)}
-                onChange={this.onChange}
-                required
-              />
-            </div>
-            <div className="text-input">
-              <label>*Password </label>
-              <input
-                className={
-                  shouldBeError("password", errors, touched) ? "error" : null
-                }
-                type="text"
-                name="password"
-                value={password}
-                onBlur={this.handleBlur("password", errors, touched)}
-                onChange={this.onChange}
-                required
-                style={this.state.passwordError ? ifPasswordError : null}
-              />
-              <span className="errorMessage">
-                {this.state.error ? errorMessage : ""}
-              </span>
-            </div>
+  const spanStyle = {
+    color: "whiteSmoke",
+    fontStyle: "italic",
+    fontSize: "0.8em",
+  };
+  const ifPasswordError = {
+    border: "2px solid red",
+  };
 
-            <p style={spanStyle}>* indicates required field</p>
-
-            <button
-              id="register-button"
-              type={!isEnabled ? "disabled" : "submit"}
-              disabled={!isEnabled}
-            >
-              Submit
-            </button>
-          </form>
-          <div className="register-push" />
-        </div>
+  const validationErrors = validateRegisration(
+    first_name,
+    last_name,
+    username,
+    password,
+    email
+  );
+  const isEnabled = !Object.keys(validationErrors).some(x => errors[x]);
+  
+  const errorMessage = validatePassword();
+  return (
+    <div className="signup-container">
+      <div className="call-to-action">
+        <h2>Join the Livemodo community!</h2>
       </div>
-    );
-  }
+      <div className="signup-form">
+        <form id="signup" onSubmit={handleSubmit}>
+          <div className="text-input">
+            <label>*First Name </label>
+            <input
+              className={
+                shouldBeError("first_name", validationErrors, touched) ? "error" : null
+              }
+              type="text"
+              name="first_name"
+              placeholder="e.g Tommy"
+              value={first_name}
+              onBlur={handleBlur("first_name", validationErrors, touched)}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="text-input">
+            <label>*Last Name </label>
+            <input
+              className={
+                shouldBeError("last_name", validationErrors, touched) ? "error" : null
+              }
+              type="text"
+              name="last_name"
+              placeholder="e.g Wisseau"
+              value={last_name}
+              onBlur={handleBlur("last_name", validationErrors, touched)}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="text-input">
+            <label>*Username </label>
+            <input
+              className={
+                shouldBeError("username", validationErrors, touched) ? "error" : null
+              }
+              type="text"
+              name="username"
+              value={username}
+              onBlur={handleBlur("username", validationErrors, touched)}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="text-input">
+            <label>*Email </label>
+            <input
+              className={
+                shouldBeError("email", validationErrors, touched) ? "error" : null
+              }
+              type="text"
+              name="email"
+              placeholder="e.g tommy@theroom.com"
+              value={email}
+              onBlur={handleBlur("email", validationErrors, touched)}
+              onChange={onChange}
+              required
+            />
+          </div>
+          <div className="text-input">
+            <label>*Password </label>
+            <input
+              className={
+                shouldBeError("password", validationErrors, touched) ? "error" : null
+              }
+              type="text"
+              name="password"
+              value={password}
+              onBlur={handleBlur("password", validationErrors, touched)}
+              onChange={onChange}
+              required
+              style={passwordError ? ifPasswordError : null}
+            />
+            <span className="errorMessage">
+              {error ? errorMessage : ""}
+            </span>
+          </div>
+
+          <p style={spanStyle}>* indicates required field</p>
+          <button
+            id="register-button"
+            type={!isEnabled ? "disabled" : "submit"}
+            disabled={!isEnabled}
+          >
+            Submit
+          </button>
+        </form>
+        <div className="register-push" />
+      </div>
+    </div>
+  );
 }
 
 export default withRouter(SignupForm);
