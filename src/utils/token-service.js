a889ab0b9466401a12479ceec9837e20ff8050d4
Ben Cohen <benjcohen1985@gmail.com>
2021-05-08 18:41:13 +0300

    Remove unused method

--- src/utils/token-service.js
+++ src/utils/token-service.js
@@ -1,21 +1,18 @@
 import config from "../config";
 
 export const TokenService = {
-  saveAuthToken(token) {
-    window.localStorage.setItem(config.API_TOKEN, token);
-  },
-  makeBasicAuthToken(username, password) {
-    return window.btoa(`${username}:${password}`);
+  
+  makeBasicAuthToken(email, password) {
+    return window.btoa(`${email}:${password}`);
   },
   clearAuthToken() {
-    window.localStorage.removeItem(config.API_TOKEN);
-    console.info("clearing auth token");
+    return window.localStorage.clear();   
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken();
   },
 
-  getAuthToken(token) {
-    return window.localStorage.getItem(config.API_TOKEN);
+  getAuthToken() {
+    return window.localStorage.getItem("jwt token");
   }
 };
