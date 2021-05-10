f4679e95faf864a780fb96a7bf53c93650469742
Ben Cohen <benjcohen1985@gmail.com>
2021-05-10 20:06:53 +0300

    Add validation Style spinner

--- src/SignupForm/SignupForm.js
+++ src/SignupForm/SignupForm.js
@@ -2,122 +2,27 @@ import React, { useState } from "react";
 import "./SignupForm.css";
 import AuthApiService from "../services/auth-api-service";
 import { withRouter } from "react-router-dom";
-// import { inputIsEmpty, shouldBeError } from "../Validation/validation";
+import { TextField } from "formik-material-ui";
+import { Formik, Form, Field } from "formik";
 import { makeStyles } from "@material-ui/core/styles";
 import {
-  TextField,
   Button,
   Container,
   FormControlLabel,
   Checkbox,
+  CircularProgress,
 } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 
-function SignupForm(props) {
-  const [input, setInput] = useState({
-    first_name: "",
-    last_name: "",
-    username: "",
-    email: "",
-    password: "",
-    repeatPassword: "",
-  });
-  const {
-    email,
-    password,
-    first_name,
-    last_name,
-    username,
-    repeatPassword,
-  } = input;
-  const [touched, setTouched] = useState({
-    first_name: false,
-    last_name: false,
-    username: false,
-    email: false,
-    password: false,
-    repeatPassword: false,
-  });
+function SignupForm({ history }) {
   const [checked, setChecked] = useState(Boolean);
-  const [errors, setErrors] = useState({
-    first_name: "",
-    last_name: "",
-    username: "",
-    email: "",
-    password: "",
-    repeatPassword: "",
-  });
-  const [hasErrors, setHasErrors] = useState(false);
-
-  const onChange = e => {
-    const { name, value } = e.target;
-
-    setInput(input => ({ ...input, [name]: value }));
-  };
-
-  const validate = e => {
-    const passwordValid = new RegExp(
-      /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/
-    ).test(password);
-
-    if (password.length < 6) {
-      setErrors({
-        password: "Password must be at least six characters long",
-      });
-    } else {
-      if (!passwordValid) {
-        setErrors({
-          password:
-            "Password must contain at least one upper case, lower case, number and special character",
-        });
-      } else {
-        setErrors({ password: "" });
-      }
-    }
-    if (username.length < 3) {
-      setErrors({
-        username: "username must be at least three characters long",
-      });
-    } else {
-    }
-    if (repeatPassword !== password) {
-      setErrors({ repeatPassword: "Passwords don't match" });
-    }
-  };
-
-  const handleSubmit = async e => {
-    const invalidInputMessage = validate();
-    e.preventDefault();
-    try {
-      if (invalidInputMessage) {
-        setHasErrors(true);
-        console.log("error");
-      } else {
-        setHasErrors(false);
-        console.log("ok");
-        AuthApiService.postUser({
-          first_name,
-          last_name,
-          username,
-          password,
-          email,
-        });
-        props.history.push("thank-you");
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const handleBlur = field => {
-    setTouched({ ...touched, [field]: true });
-  };
+  const [hasErrors, setHasErrors] = useState({});
 
   const useStyles = makeStyles(theme => ({
     paper: {
       marginTop: theme.spacing(8),
-      marginBottom: 200,
+      marginBottom: 100,
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
@@ -133,132 +38,217 @@ function SignupForm(props) {
   }));
   const classes = useStyles();
 
-  const handleCheckbox = e => {
+  const handleCheckbox = () => {
     setChecked(checked => !checked);
   };
+  // show any error message if field is populated
+  const errorMessage = Object.values(hasErrors).filter(err => err.length !== 0);
 
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h5" style={{ margin: "10% 0 15%" }}>
           Join the Livemodo community
         </Typography>
-        <form className={classes.form} onSubmit={handleSubmit}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                error={touched.first_name && first_name.length === 0}
-                name="first_name"
-                onBlur={() => handleBlur("first_name")}
-                variant="outlined"
-                value={first_name}
-                required
+
+        <Formik
+          initialValues={{
+            first_name: "",
+            last_name: "",
+            username: "",
+            email: "",
+            password: "",
+            repeatPassword: "",
+          }}
+          enableReinitialize
+          validate={values => {
+            const {
+              email,
+              first_name,
+              last_name,
+              username,
+              password,
+              repeatPassword,
+            } = values;
+            const errors = {};
+            // // validate email
+            if (!email) {
+              errors.email = "Required";
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email)
+            ) {
+              errors.email = "Invalid email address";
+            }
+            // // // validate username
+            if (!username) {
+              errors.username = "Username is required";
+            } else if (username.length < 2) {
+              errors.username = "Username must be at least two characters.";
+            }
+            // // validate password
+            if (!password) {
+              errors.password = "Password is required.";
+            } else if (
+              !/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])[\S]+/.test(
+                password
+              )
+            ) {
+              errors.password =
+                "Password must contain at least one upper case, lower case, number and special character.";
+            } else if (password.length < 6) {
+              errors.password = "Password must be at least six characters.";
+            }
+            // validate repeat password
+            if (repeatPassword.trim() === "") {
+              errors.repeatPassword = "Please type in password again.";
+            }
+            if (password !== repeatPassword) {
+              errors.repeatPassword = "Passwords don't match";
+            }
+            // validate first name
+            if (first_name.trim() === "") {
+              errors.first_name = `First name is required`;
+            } else if (/[^a-zA-Z -]/.test(first_name)) {
+              errors.first_name = "Invalid characters";
+            } else if (first_name.trim().length < 3) {
+              errors.first_name = `First name needs to be at least three characters`;
+            }
+            // validate last name
+            if (last_name.trim() === "") {
+              errors.last_name = `First name is required`;
+            } else if (/[^a-zA-Z -]/.test(last_name)) {
+              errors.last_name = "Invalid characters";
+            } else if (last_name.trim().length < 3) {
+              errors.last_name = `First name needs to be at least three characters`;
+            }
+
+            return errors;
+          }}
+          onSubmit={async (values, actions) => {
+            actions.setSubmitting(true);
+            let apiCall = await AuthApiService.postUser({
+              first_name: values.first_name.trim(),
+              last_name: values.last_name.trim(),
+              username: values.username.trim(),
+              password: values.password.trim(),
+              email: values.email.trim(),
+            });
+            if (apiCall.errors) {
+              setHasErrors(apiCall.errors);
+              actions.setSubmitting(false);
+            } else {
+              actions.setSubmitting(true);
+              history.push("/");
+            }
+          }}
+        >
+          {({ submitForm, isSubmitting, isValid }) => (
+            <Form>
+              <div>
+                {isSubmitting && (
+                  <CircularProgress
+                    size={60}
+                    variant="indeterminate"
+                    id="spinner"
+                  />
+                )}
+              </div>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    name="first_name"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="last_name"
+                    label="Last Name"
+                    name="last_name"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    type="email"
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email Address"
+                    name="email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="password"
+                    helperText="Must contain uppercase, lowercase, numbers and special characters"
+                    label="Password"
+                    type={checked ? "text" : "password"}
+                    id="password"
+                  />
+
+                  <FormControlLabel
+                    value="Show password"
+                    control={<Checkbox color="primary" />}
+                    label="Show password"
+                    checked={checked}
+                    onChange={handleCheckbox}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Field
+                    component={TextField}
+                    variant="outlined"
+                    required
+                    fullWidth
+                    name="repeatPassword"
+                    label="Repeat Password"
+                    type="password"
+                    id="passwordRepeat"
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                onClick={submitForm}
+                type="submit"
                 fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-                onChange={onChange}
-              />
-              <p style={{ color: "red" }}>{errors.first_name}</p>
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                error={touched.last_name && last_name.length === 0}
-                onBlur={() => handleBlur("last_name")}
-                variant="outlined"
-                value={last_name}
-                required
-                helperText={errors.last_name}
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="last_name"
-                onChange={onChange}
-              />
-              <p style={{ color: "red" }}>{errors.last_name}</p>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                onBlur={() => handleBlur("email")}
-                error={touched.email && email.length === 0}
-                variant="outlined"
-                // type="email"
-                value={email}
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                onChange={onChange}
-              />
-              <p style={{ color: "red" }}>{errors.email}</p>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                onBlur={() => handleBlur("username")}
-                error={touched.username && username.length === 0}
-                variant="outlined"
-                required
-                value={username}
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                onChange={onChange}
-              />
-              <p style={{ color: "red" }}>{errors.username}</p>
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                onBlur={() => handleBlur("password")}
-                error={touched.password && password.length === 0}
-                variant="outlined"
-                required
-                value={password}
-                fullWidth
-                name="password"
-                helperText="Must contain uppercase, lowercase, numbers and special characters"
-                label="Password"
-                type={checked ? "text" : "password"}
-                id="password"
-                onChange={onChange}
-              />
-              <p style={{ color: "red" }}>{errors.password}</p>
-              {password}
-              <FormControlLabel
-                value="Show password"
-                control={<Checkbox color="primary" />}
-                label="Show password"
-                checked={checked}
-                onChange={handleCheckbox}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                onBlur={() => handleBlur("repeatPassword")}
-                variant="outlined"
-                value={repeatPassword}
-                required
-                fullWidth
-                name="repeatPassword"
-                label="Repeat Password"
-                type="password"
-                id="passwordRepeat"
-                onChange={onChange}
-              />
-              <p style={{ color: "red" }}>{errors.repeatPassword}</p>
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="default"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-        </form>
+                disabled={!isValid || isSubmitting}
+                variant="contained"
+                color="default"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+            </Form>
+          )}
+        </Formik>
+        <p style={{ color: "red" }}>{errorMessage}</p>
       </div>
     </Container>
   );
