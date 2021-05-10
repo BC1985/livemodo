92db272a6ca4c77714a37321a38ae449455a5069
Ben Cohen <benjcohen1985@gmail.com>
2021-05-10 23:16:08 +0300

    Import Material nav

--- src/NavBar/Navbar.js
+++ src/NavBar/Navbar.js
@@ -1,38 +1,64 @@
 import React, { Component } from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import "./Navbar.css";
+import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core/";
+
 import DrawerToggleButton from "../DrawerToggleBottun/DrawerToggleButon";
-class Navbar extends Component {
-  render() {
-    return (
-      <div className="container">
-        <nav className="nav-wrapper" role="navigation">
-          <div className="hamburger-icon">
-            <DrawerToggleButton
-              isSideDrawerOpen={this.props.isSideDrawerOpen}
-              click={this.props.drawerToggleClickHandler}
-            />
-          </div>
-          <div id="logo">LIVEMODO</div>
-          <div className="spacer" />
-          <div className="nav-links">
-            <ul>
-              {this.props.routes.map(route => (
-                <li key={route.name}>
-                  <NavLink
-                    to={route.to}
-                    onClick={route.onClick}
-                  >
-                    {route.name}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
-}
+import { makeStyles } from "@material-ui/core/styles";
+
+const Navbar = ({ routes, isSideDrawerOpen, drawerToggleClickHandler }) => {
+  const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+    
+  }));
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar disableGutters={true} disablePadding={true}>
+        <div className="container">
+          <nav className="nav-wrapper" role="navigation">
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <div className="hamburger-icon">
+                <DrawerToggleButton
+                  isSideDrawerOpen={isSideDrawerOpen}
+                  click={drawerToggleClickHandler}
+                />
+              </div>
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+            Livemodo
+          </Typography>
+            <div className="spacer" />
+            <div className="nav-links">
+              <ul>
+                {routes.map(route => (
+                  <li key={route.name}>
+                    <NavLink to={route.to} onClick={route.onClick}>
+                      {route.name}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </nav>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default withRouter(Navbar);
