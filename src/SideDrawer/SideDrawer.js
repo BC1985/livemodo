d83a3f4fd0090c9c0fb295262b62a9fbef59576f
Ben Cohen <benjcohen1985@gmail.com>
2021-05-11 21:17:22 +0300

    Destructure props

--- src/SideDrawer/SideDrawer.js
+++ src/SideDrawer/SideDrawer.js
@@ -2,15 +2,15 @@ import React from "react";
 import "./SideDrawer.css";
 import { NavLink } from "react-router-dom";
 
-const sideDrawer = props => {
+const sideDrawer = ({show, routes}) => {
   let drawerClasses = "side-drawer";
-  if (props.show) {
+  if (show) {
     drawerClasses = "side-drawer open";
   }
   return (
     <nav className={drawerClasses}>
       <ul>
-        {props.routes.map(route => (
+        {routes.map(route => (
           <li key={route.name}>
             <NavLink to={route.to} onClick={route.onClick}>
               {route.name}
