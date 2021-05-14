ab009cac748f956a0690e28ed479e074685d10f8
Ben Cohen <benjcohen1985@gmail.com>
2021-05-14 11:43:57 +0300

    Wrap in theme provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import {
+  createMuiTheme,
+  ThemeProvider,
+} from "@material-ui/core/styles";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const theme = createMuiTheme({
+    palette: {
+      primary:{
+          main:"#482880"
+      },secondary:{
+          main:"#4a148c"
+      }
+    },
+  });
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
