e7145e8bcae0155eea095901986cf4752e232ca4
Ben Cohen <benjcohen1985@gmail.com>
2021-05-14 21:49:22 +0300

    Minimize props

--- src/BrowseReviews/BrowseReviewsPage.js
+++ src/BrowseReviews/BrowseReviewsPage.js
@@ -6,44 +6,32 @@ import Spinner from "../Spinners/Spinner";
 
 function BrowseReviewsPage() {
   const [reviews, setReviews] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  useEffect(() =>{
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
     const fetchReviews = async () => {
       try {
         const data = await fetch(`${config.API_BASE_URL}/reviews/all`);
         const reviews = await data.json();
-        setReviews(reviews);   
-        console.log(reviews)
+        setReviews(reviews);
         setIsLoading(false);
       } catch (e) {
         console.log(e);
       }
     };
     fetchReviews();
-  },[])
-    
-    const allReviews = reviews.map(review => (
-      <Review
-        key={review._id}
-        bandName={review.bandName}
-        showDate={review.showDate}
-        tagline={review.tagline}
-        createdAt={review.createdAt}
-        venue={review.venue}
-        content={review.content}
-        username={review.username}
-        rating={review.rating}
-      />
-    ));
-    return (
-      <div className="reviews-container">
-        {isLoading ? <Spinner /> : null}
-        <main role="main">
-          <h1>Posted reviews</h1>
-          <div className="review">{allReviews}</div>
-        </main>
-      </div>
-    );
-  
+  }, [])
+
+  const allReviews = reviews.map(review => (
+    <Review key={review._id} review={review} />
+  ));
+  return (
+    <div className="reviews-container">
+      {isLoading ? <Spinner /> : null}
+      <main role="main">
+        <h1>Posted reviews</h1>
+        <div className="review">{allReviews}</div>
+      </main>
+    </div>
+  );
 }
 export default BrowseReviewsPage;
