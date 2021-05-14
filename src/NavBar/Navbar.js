a7b38f5550e36c4c33b9bee25aa75e02d5426635
Ben Cohen <benjcohen1985@gmail.com>
2021-05-14 12:00:19 +0300

    Optimize drawer

--- src/NavBar/Navbar.js
+++ src/NavBar/Navbar.js
@@ -1,60 +1,36 @@
 import React from "react";
 import { NavLink, withRouter } from "react-router-dom";
 import "./Navbar.css";
-import { AppBar, Toolbar, IconButton, Typography } from "@material-ui/core/";
-
-// import DrawerToggleButton from "../DrawerToggleBottun/DrawerToggleButon";
+import { AppBar, Toolbar, Typography } from "@material-ui/core/";
 import { makeStyles } from "@material-ui/core/styles";
+import SideDrawer from "../SideDrawer/SideDrawer";
 
-const Navbar = ({ routes, isSideDrawerOpen, drawerToggleClickHandler }) => {
-  const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
+const Navbar = ({ routes, isLoggedIn, authenticateRoutes }) => {
+  const useStyles = makeStyles(() => ({
     title: {
       flexGrow: 1,
     },
-    
   }));
   const classes = useStyles();
 
   return (
     <AppBar position="static">
       <Toolbar>
-        <div className="container">
-          <nav className="nav-wrapper" role="navigation">
-            {/* <IconButton
-              edge="start"
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="menu"
-            >
-              <div className="hamburger-icon">
-                <DrawerToggleButton
-                  isSideDrawerOpen={isSideDrawerOpen}
-                  click={drawerToggleClickHandler}
-                />
-              </div>
-            </IconButton> */}
-            <Typography variant="h6" className={classes.title}>
-            Livemodo
-          </Typography>
-            <div className="spacer" />
-            <div className="nav-links">
-              <ul>
-                {routes.map(route => (
-                  <li key={route.name}>
-                    <NavLink to={route.to} onClick={route.onClick}>
-                      {route.name}
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </nav>
+        <SideDrawer routes={isLoggedIn ? authenticateRoutes : routes} />
+        <Typography variant="h6" className={classes.title}>
+          Livemodo
+        </Typography>
+        <div className="spacer" />
+        <div className="nav-links">
+          <ul>
+            {routes.map(route => (
+              <li key={route.name}>
+                <NavLink to={route.to}>
+                  {route.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
         </div>
       </Toolbar>
     </AppBar>
--- src/SideDrawer/SideDrawer.js
+++ src/SideDrawer/SideDrawer.js
@@ -1,25 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SideDrawer.css";
 import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import { List, ListItem, IconButton, Drawer } from "@material-ui/core/";
 
-const sideDrawer = ({show, routes}) => {
-  let drawerClasses = "side-drawer";
-  if (show) {
-    drawerClasses = "side-drawer open";
-  }
-  return (
-    <nav className={drawerClasses}>
-      <ul>
+const SideDrawer = ({ routes }) => {
+  const useStyles = makeStyles({
+    list: {
+      width: 200,
+    },
+  });
+
+  const classes = useStyles();
+  const [state, setState] = useState(false);
+  const toggleDrawer = open => event => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+    setState(open);
+  };
+  const list = () => (
+    <div
+      className={classes.list}
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
         {routes.map(route => (
-          <li key={route.name}>
+          <ListItem button key={route.name}>
             <NavLink to={route.to} onClick={route.onClick}>
               {route.name}
             </NavLink>
-          </li>
+          </ListItem>
         ))}
+      </List>
+    </div>
+  );
+  return (
+    <nav>
+      <ul>
+        <IconButton
+          onClick={toggleDrawer(true)}
+          edge="start"
+          size="small"
+          color="inherit"
+          aria-label="menu"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Drawer open={state} onClose={toggleDrawer(false)}>
+          {list()}
+        </Drawer>
       </ul>
     </nav>
   );
 };
 
-export default sideDrawer;
+export default SideDrawer;
